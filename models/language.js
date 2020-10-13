3a8873f4228b15ba827619ed4d7a5fe026a8bb2b
Serin <serin0837@yahoo.com>
2020-10-13 13:07:02 +0100

    change language schema

--- models/language.js
+++ models/language.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const languageSchema = new Schema({
-  language:{
+  languagename:{
     type:String,
     required:true,
     unique:true,
--- models/word.js
+++ models/word.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WordSchema = new Schema({
-  language:{
+  languagename:{
     type:String,
   },
   name: {
