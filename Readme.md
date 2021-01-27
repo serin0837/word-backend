049fc82193fc8e16c4300f045a6e0d0daf2f2d89
Serin <serin0837@yahoo.com>
2021-01-27 19:12:31 +0000

    refactor the code and change reademe

--- Readme.md
+++ Readme.md
@@ -1,8 +1,21 @@
-# Description
+## Description
 
-This is back end made with Express and MongoDB.
+Backend made with Express and MongoDB & Mongoose.
 
-## End points
+## API Endpoint
 
-/words -> Get, Post
-/word/id -> Delete, Update
+- /words -> get, post
+- /word/:id -> delete, put(update)
+- /languages -> get, post
+- /language/:id -> delete
+- /words/userId/:userId -> get
+
+## Technologies
+
+- Front end: Responsive website built with React, CSS and Google OAuth.
+
+  - [🔗Live](https://word-card.netlify.app/)
+  - [💻Code](https://github.com/serin0837/word-list-app)
+
+- Back end: Server made with Express and MongoDB.
+  - [🔗Live](https://word-back.herokuapp.com/api/words)
--- routes/api.js
+++ routes/api.js
@@ -10,26 +10,26 @@ router.get("/words", function (req, res) {
   });
 });
 //get word with user id
-router.get("/words/userId/:userId",function(req,res){
+router.get("/words/userId/:userId", function(req,res){
   Word.find({userId:req.params.userId}).then(function (word) {
     res.send(word);
   });
 })
 
-// add a word to db -> working!!!!
+// add a word to db 
 router.post("/words", function (req, res) {
   //new instance of County object
   Word.create(req.body).then(function (word) {
     res.send(word);
   });
 });
-//delete word by id //working
+//delete word by id 
 router.delete("/words/:id", function (req, res) {
   Word.findByIdAndRemove({ _id: req.params.id }).then(function (word) {
     res.send(word);
   });
 });
-//update word by id//working
+//update word by id
 router.put("/words/:id", function (req, res, next) {
   Word.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
     function (word) {
@@ -59,41 +59,4 @@ router.delete("/languages/:id", function (req, res) {
   });
 });
 
-
-//get every word
-// app.get("/word", (req, res) => {
-//   console.log("received");
-//   res.contentType("application/json");
-//   res.setHeader("access-control-allow-origin", "*");
-//   res.status(200).send(words);
-// });
-
-// //post new word
-// app.post("/word", (req, res) => {
-//   console.log(req.body);
-//   const wordToAdd = {
-//     id: uuid.v4(),
-//     name: req.body.name,
-//     meaning: req.body.meaning,
-//     note: req.body.note,
-//     remember: false,
-//   };
-//   words.push(wordToAdd);
-//   res.contentType("application/json");
-//   res.setHeader("access-control-allow-origin", "*");
-//   res.status(201).send(wordToAdd);
-// });
-// // delete request
-// app.delete("/word", (req, res) => {
-//   console.log("id");
-//   res.contentType("application/json");
-//   res.setHeader("access-control-allow-origin", "*");
-//   res.status(204);
-
-//   words.filter((word) => {
-//     return word.id !== req.body.id;
-//   });
-// });
-
-// words: currentState.words.filter(
 module.exports = router;
